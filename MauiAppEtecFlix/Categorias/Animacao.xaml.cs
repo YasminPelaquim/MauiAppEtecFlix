6a5472f31e7392bdc2daed854903f2ed5baaa082
Usuario <Usuario@CPD03-M16>
2024-05-27 15:00:50 -0300

    27/05

--- MauiAppEtecFlix/Categorias/Animacao.xaml.cs
+++ MauiAppEtecFlix/Categorias/Animacao.xaml.cs
@@ -9,12 +9,28 @@ public partial class Animacao : ContentPage
 
     private void btnWalle_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.walle());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnAguaabaixo_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.aguaabaixo());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnLuca_Clicked(object sender, EventArgs e)
