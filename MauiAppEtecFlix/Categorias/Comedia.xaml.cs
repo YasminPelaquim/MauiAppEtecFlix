cd920048ee4e18b9334180892f1074f71f851d0d
yasmi <yasmi@YASMIN>
2024-06-02 17:30:38 -0300

    20/06-17:30

--- MauiAppEtecFlix/Categorias/Comedia.xaml.cs
+++ MauiAppEtecFlix/Categorias/Comedia.xaml.cs
@@ -6,4 +6,52 @@ public partial class Comedia : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnGente_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Filmes.gente());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void btnParis_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Filmes.paris());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void btnJuntos_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Filmes.juntos());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void btnSexta_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Filmes.sexta());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
--- MauiAppEtecFlix/Categorias/Drama.xaml.cs
+++ MauiAppEtecFlix/Categorias/Drama.xaml.cs
@@ -6,4 +6,34 @@ public partial class Drama : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnHistorias_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnHistorias_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnHistorias_Clicked_2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnAdoraveis_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnBaleia_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnUmlugar_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
